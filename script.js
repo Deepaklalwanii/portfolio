b81d8d513f5e78f9afa8023f3765bde70a1fa3aa
deepaklalwanii <deepaklalwani752@gmail.com>
2024-11-07 16:02:38 +0530

    google-analytics

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 window.addEventListener('DOMContentLoaded', function () {
-    let project = document.querySelector('#project');
-    function onclick(keyup){
-        
+    const to_glow = document.querySelectorAll('.glow');  
+    const projectSection = document.querySelector('.projects');
+    const aboutHeader = document.querySelector('.lft-box-two');
+    
+    
+    to_glow.forEach(function (element) {
+      element.addEventListener('click', function () {
+        showData(element);
+      });
+    });
+    
+    function showData(element) {
+      
+      if (element.dataset.box === 'project') {
+        projectSection.classList.add('background-glow'); 
+        setTimeout(function() {
+            projectSection.classList.remove('background-glow');
+          }, 2000); 
+      } else if (element.dataset.box === 'about') {
+        aboutHeader.classList.add('background-glow');
+        setTimeout(function() {
+            aboutHeader.classList.remove('background-glow');  // Remove the class after 3 seconds
+          }, 2000);
+      }
+
     }
-
-
-
- });
+  });
+  
